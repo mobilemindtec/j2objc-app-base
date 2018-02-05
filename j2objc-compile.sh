a2ea64c545fd76ae7ad4fe80b480fa604b2b153d
Ricardo Bocchi <ricardo@mobilemind.com.br>
2018-02-05 18:25:03 -0200

    changes

--- j2objc-compile.sh
+++ j2objc-compile.sh
@@ -7,26 +7,30 @@ mkdir -p $BUILD/{java,objc,bin}
 
 find "./src/main" -name '*.java' -type f -exec cp {} "$BUILD/java/" \;
 find "./src/main" -type f -name 'packages.properties' -exec cat {} + >> "$BUILD/java/packages.properties" ;
-find "../j2objc-json/src/main" -type f -name 'packages.properties' -exec cat {} + >> "$BUILD/java/packages.properties" ;
+find "../j2objc-gson/src/main" -type f -name 'packages.properties' -exec cat {} + >> "$BUILD/java/packages.properties" ;
 
 CLASS_PATH="./src/main/java:../json/src/main/java"
 
 for f in $BUILD/java/*.java; do
 
-	echo $f
   FILE_NAME=`basename $f`
 	NAME=${FILE_NAME//".java"/""}
 	FILE_M="${NAME}.m"
+
+	echo "** j2objc compile $FILE_NAME"
 
 	j2objc -d $BUILD/objc \
     -sourcepath "$BUILD/java/**.java" \
     -classpath $CLASS_PATH \
     -use-arc --prefixes $BUILD/java/packages.properties \
     --swift-friendly \
-    --verbose \
     --no-package-directories $f
 
 done
 
-rm -R ios/Classes/*
-cp $BUILD/objc/* ios/Classes
+
+## prepare and copy ios sources to pod project
+
+APP_IOS_SOURCES=ios/J2ObjCAppBase/Classes
+rm -R $APP_IOS_SOURCES/*
+cp $BUILD/objc/* $APP_IOS_SOURCES/
